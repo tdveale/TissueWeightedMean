411f21d2a1a6e408fa15bc66847d5509767f2fc3
Tom Veale <tom.veale@ucl.ac.uk>
2020-10-21 16:25:59 +0100

    working script to do code in the tutorial - calculate modulated NODDI and tissue fraction, extract ROIs into spreadsheet

--- scripts/calculate_tissue_weighted_rois.sh
+++ scripts/calculate_tissue_weighted_rois.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Script that calculates tissue weighted ROIs from NODDI inputs and ROI images
-
 # collect inputs - make these names inputs instead of by position
 iso=$1
 ndi=$2
@@ -10,6 +9,38 @@ mask=$4
 roiBase=$5
 outCSV=$6
 
-# use modulate_noddi.sh to create tissue fraction and modulated NDI and ODI
+# modulate noddi and create tissue fraction
+# originally wanted to make the below call ./modulate_noddi.sh but getting source directory within a script was tough - working solution for now
+if [ -e "${iso}" ]; then
+  echo "Creating tissue fraction, modulated NDI and modulated ODI maps..."
+  # calculate 1-Viso (tissue weighted)
+  fslmaths ${iso} -mul -1 -add 1 -mas ${mask} ${iso%.nii.gz}_ftissue.nii.gz
+  ls ${iso%.nii.gz}_ftissue.nii.gz
+  # multiply by ndi
+  fslmaths ${iso%.nii.gz}_ftissue.nii.gz -mul ${ndi} ${ndi%.nii.gz}_modulated.nii.gz
+  ls ${ndi%.nii.gz}_modulated.nii.gz
+  # multiply by odi
+  fslmaths ${iso%.nii.gz}_ftissue.nii.gz -mul ${odi} ${odi%.nii.gz}_modulated.nii.gz
+  ls ${odi%.nii.gz}_modulated.nii.gz
+else
+  echo "ISO IMAGE NOT FOUND"
+fi
+
+# set up spreadsheet
+echo "NODDI_METRIC,ROI,MEAN,SD" > ${outCSV}
 
 # loop through all roi images from ${roiBase} and create output csv
+for roi in *${roiBase}*.nii*; do
+  # get stats for modulated NDI
+  istats=(`fslstats ${ndi%.nii.gz}_modulated.nii.gz -k ${roi} -m -s`);
+  echo mNDI,${roi},${istats[0]},${istats[1]} >> ${outCSV}
+  # get stats for modulated ODI
+  istats=(`fslstats ${odi%.nii.gz}_modulated.nii.gz -k ${roi} -m -s`);
+  echo mODI,${roi},${istats[0]},${istats[1]} >> ${outCSV}
+  # get stats for Tissue Fraction
+  istats=(`fslstats ${iso%.nii.gz}_ftissue.nii.gz -k ${roi} -m -s`);
+  echo TissueFraction,${roi},${istats[0]},${istats[1]} >> ${outCSV}
+done
+
+echo "ROI stats saved to: ${outCSV}"
+echo "Divide modulate NODDI metrics by associated Tissue Fraction to obtain tissue-weighted ROI measures."
